ab980b2cfb168a97206f32eac09e651a47be52b2
nidhi-08 <55029966+nidhi-08@users.noreply.github.com>
2020-05-19 10:01:09 +0530

    May 9th

---
+++ 09.  Valid_Perfect_Square.java
@@ -0,0 +1,22 @@
+class Solution {
+  public boolean isPerfectSquare(int num) {
+    if (num < 2) {
+      return true;
+    }
+
+    long left = 2, right = num / 2, x, guessSquared;
+    while (left <= right) {
+      x = left + (right - left) / 2;    //midpoint
+      guessSquared = x * x;   //guess squared sort of binary search
+      if (guessSquared == num) {
+        return true;
+      }
+      if (guessSquared > num) {
+        right = x - 1;
+      } else {
+        left = x + 1;
+      }
+    }
+    return false;
+  }
+}
