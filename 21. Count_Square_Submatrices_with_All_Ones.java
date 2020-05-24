7479054513028fcbe9239f2fd574e4c6db95868b
Nidhi Anil Khare <55029966+nidhi-08@users.noreply.github.com>
2020-05-24 21:12:54 +0530

    May 21st

---
+++ 21. Count_Square_Submatrices_with_All_Ones.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int res = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = matrix[i][j];
+                }
+                else{
+                    dp[i][j] = matrix[i][j] == 1 ? Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1])) + 1 : 0;
+                }
+                res += dp[i][j];
+            }
+        }
+        
+        return res;
+    }
+}
