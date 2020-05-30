4a4d15c28baa3ac1cfe11acfe69ae15cd95e8886
Nidhi Anil Khare <55029966+nidhi-08@users.noreply.github.com>
2020-05-31 00:40:42 +0530

    May 30th

---
+++ 30. K_Closest_Points_to_Origin.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[0]*b[0]+b[1]*b[1]) - (a[0]*a[0]+a[1]*a[1]));
+        for (int[] p : points) 
+        {
+            maxHeap.offer(p);
+            if (maxHeap.size() > K) 
+            {
+                maxHeap.poll();
+            }
+        }
+        int[][] ans = new int[maxHeap.size()][2];
+        int i = 0;
+        while (!maxHeap.isEmpty()) {
+            ans[i++] = maxHeap.poll();
+        }
+        return ans;
+    }
+}
